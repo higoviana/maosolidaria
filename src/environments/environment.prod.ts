2fbb07433e523cd9f02c823a14bebe40635cf279
higoviana <higoalves34@gmail.com>
2022-05-17 00:04:27 -0300

    dist biuld

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyCx4vQgZnfoBcjrfz4bsOmdIRt9dXcTzyo",
+    authDomain: "doador-c7b2f.firebaseapp.com",
+    projectId: "doador-c7b2f",
+    storageBucket: "doador-c7b2f.appspot.com",
+    messagingSenderId: "1015178175804",
+    appId: "1:1015178175804:web:34cb468648a1082405ea39"
+    }
 };
